81a522ef0a429015f6c4cb6a2c855902731effe9
yatindrarao <yatindrarao.rao@gmail.com>
2017-01-02 14:25:14 +0530

    Adds rateLimit of map location call for development

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@ function ViewModel(){
   var self = this;
   self.locations = getLocations(["Jaipur", "Bangalore", "Ahmedabad", "Pune", "Hyderabad"]);
   self.searchLocation = ko.observable();
+  self.searchLocation.extend({rateLimit: 1500}) //To be commented when run on production
   defaultMarkers(self.locations);
 
   self.searchLocation.subscribe(function(newVal){
     // deleteMarkers();
     if(newVal.length > 0){
       updateMap(newVal);
+    }
+    else{
+      deleteMarkers();
     }
   })
 };
